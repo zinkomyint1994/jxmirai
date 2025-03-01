2be06e9e9a944bc240daf0be2b213f2d8a8ef357
amy-otechmm <amy@o-technique-myanmar.com>
2025-03-01 11:14:59 +0630

    slider and product page

--- wp-content/themes/jxmirai/page-manufacture.php
+++ wp-content/themes/jxmirai/page-manufacture.php
@@ -1,174 +1,201 @@
-<?php 
+<?php
+
 /**
  * Template Name: page-manufacture
  */
 get_header(); ?>
 <!-- main-banner -->
 <?php
-   global $title;
-   global $caption;
-   global $bgColor;
-   $title = "MANUFACTURING BASE";
-   $img = "当社のサービス";
-   $bgColor = '#fff';
-   $url = "/assets/images/banner/manufacturebanner.png";
+global $title;
+global $caption;
+global $bgColor;
+$title = "MANUFACTURING BASE";
+$img = "当社のサービス";
+$bgColor = '#fff';
+$url = "/assets/images/banner/manufacturebanner.png";
 include get_template_directory() . '/custom/pagesBanner.php';
 ?>
 
 <!-- factories-japan -->
-<div  class="factories_wrapper">
+<div class="factories_wrapper">
+  <div>
+    <?php
+    global $title;
+    $title = "Factories from Japan";
+    include get_template_directory() . '/custom/subTitle.php';
+    ?>
+  </div>
+  <!-- container -->
+  <div class="factories_container">
     <div>
-        <?php
-        global $title;
-        $title = "Factories from Japan";
-                include get_template_directory() . '/custom/subTitle.php';
-        ?>
+      <div class="slider-container">
+        <div class="main-image">
+          <img  src="<?php echo get_template_directory_uri(); ?>/assets/images/manufacture/Picture1.png" alt="Main Image">
         </div>
-   <!-- container -->
-    <div class="factories_container">
-            <div>
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/banner/banner.png" alt="Example Image" class="mf_img">
-            </div>
-            <div class="mf_list_wrap">
-                <div class="mf_title">Xiamen Xiechangyuan Mold Co., Ltd.</div>
-                    <ul class="ul_flex">
-                            <li>Location: Xiamen, China</li>
-                            <li>Main Operations: Injection mold manufacturing and molding</li>
-                            <li>Established: April 2000</li>
-                            <li>Factory Size: 12,000 m² facility, 130 employees</li>
-                            <li>Certifications: ISO 9001 & ISO 14001</li>
-                            <li>Key Equipment: Injection molding machines (10-1,300 tons, 60 units)</li>
-                            <li>Major Clients: TOYOTA, LG, Midea, Haier, TOTO</li>
-                    </ul>
-            <div class="mf_blur"></div>
-            </div>
+
+        <div class="thumbnails">
+          <img class="thumb active" src="<?php echo get_template_directory_uri(); ?>/assets/images/manufacture/Picture1.png" alt="Thumb 1" onclick="changeImage(this)">
+          <img class="thumb" src="<?php echo get_template_directory_uri(); ?>/assets/images/manufacture/Picture2.png" alt="Thumb 2" onclick="changeImage(this)">
+          <img class="thumb" src="<?php echo get_template_directory_uri(); ?>/assets/images/manufacture/Picture3.png" alt="Thumb 3" onclick="changeImage(this)">
+          <img class="thumb" src="<?php echo get_template_directory_uri(); ?>/assets/images/manufacture/Picture4.png" alt="Thumb 4" onclick="changeImage(this)">
+        </div>
+      </div>
+
     </div>
-    <!-- container -->
-    <div class="spreator"></div>
-       <!-- container -->
-       <div class="factories_container">
-            <div>
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/banner/banner.png" alt="Example Image" class="mf_img">
-            </div>
-            <div class="mf_list_wrap">
-                <div class="mf_title">Xiamen Ouli Industrial & Trade Co., Ltd.</div>
-                    <ul class="ul_flex">
-                            <li>Location: Xiamen, China</li>
-                            <li>Main Operations: Injection mold manufacturing and molding</li>
-                            <li>Established: April 2000</li>
-                            <li>Factory Size: 12,000 m² facility, 130 employees</li>
-                            <li>Certifications: ISO 9001 & ISO 14001</li>
-                            <li>Key Equipment: Injection molding machines (10-1,300 tons, 60 units)</li>
-                            <li>Major Clients: TOYOTA, LG, Midea, Haier, TOTO</li>
-                    </ul>
-            <div class="mf_blur"></div>
-            </div>
+    <div class="mf_list_wrap">
+      <div class="mf_title">射出成形金型製作/成形メーカー</div>
+      <ul class="ul_flex">
+        <li>工場名：厦門協晟源模具有限公司</li>
+        <li>Main Operations: Injection mold manufacturing and molding</li>
+        <li>工場建屋:12,000m 2 </li>
+        <li>現在の従業員：130名</li>
+        <li>システム認証：ISO 9001 & ISO14001</li>
+        <li>設立 ：2024/5/28 </li>
+        <li>設立年：2000年/４月</li>
+        <li>精密金型、射出成形、インサート金型、２色成形 成形機：10ton~1,300ton60機</li>
+
+      </ul>
+      <div class="mf_blur"></div>
     </div>
-    <!-- container -->
+  </div>
+  <!-- container -->
+  <div class="spreator"></div>
+  <!-- container -->
+  <div class="factories_container">
+    <div>
+      <div class="slider-container">
+        <div class="main-image">
+          <img  src="<?php echo get_template_directory_uri(); ?>/assets/images/manufacture/Picture7.png" alt="Main Image">
+        </div>
+
+        <div class="thumbnails">
+          <img class="thumb active" src="<?php echo get_template_directory_uri(); ?>/assets/images/manufacture/Picture8.png" alt="Thumb 1" onclick="changeImage(this)">
+          <img class="thumb" src="<?php echo get_template_directory_uri(); ?>/assets/images/manufacture/Picture9.png" alt="Thumb 2" onclick="changeImage(this)">
+          <img class="thumb" src="<?php echo get_template_directory_uri(); ?>/assets/images/manufacture/Picture7.png" alt="Thumb 3" onclick="changeImage(this)">
+          <img class="thumb" src="<?php echo get_template_directory_uri(); ?>/assets/images/manufacture/Picture6.png" alt="Thumb 4" onclick="changeImage(this)">
+        </div>
+      </div>
+    </div>
+    <div class="mf_list_wrap">
+      <div class="mf_title">３Dプリンター、樹脂/金属切削メーカー</div>
+      <ul class="ul_flex">
+        <li>工場名：厦門欧力工贸有限公司 </li>
+        <li>工場建屋:8,000m2 </li>
+        <li>現在の従業員：50名 </li>
+        <li>システム認証：ISO 9001 & ISO14001 </li>
+        <li>設立年：2006年/2月 </li>
+        <li>３Dプリンター、樹脂切削、金属切削、板金、注型 、治具製造 </li>
+      </ul>
+      <div class="mf_blur"></div>
+    </div>
+  </div>
+  <!-- container -->
 </div>
 <!-- factories-japan -->
 <div class="spreator"></div>
 <!-- sample -->
 <div class="sample_wrapper">
-    <!-- products-tag  -->
-<div class="sample_inner">
-                <div>
-                    <div class="sample-main-title">
-                    We Partner With Over 50 High-quality Manufacturers In China 
-                    </div>
-                    <div  class="sample_content_wrap">
-                            <div>サンプル金型の作成</div>
-                            <div class="news_title">
-                                <span>
-                                <?php
-                                include get_template_directory() . '/custom/subBlueLine.php';
-                                ?>
-                                </span>
-                                <span>試作・改良のために設計されており、本格的な生産に入る前にクライアントがデザインを検証できるよう支援します。</span>
-                            </div>
-                           
-                            
-                            <div>サンプル金型の作成</div>
-                            <div>
-                                <div class="news_title">
-                                    <span>
-                                    <?php
-                                    include get_template_directory() . '/custom/subBlueLine.php';
-                                    ?>
-                                    </span>
-                                    <span>試作・改良のために設計されており、本格的な生産に入る前にクライアントがデザインを検証できるよう支援します。</span>
-                                </div></br>
-                                <span>サンプル金型は、設計・試作段階でクライアントと密接に連携できるよう、現地で製造されます。量産用金型は、中国や日本の高度な製造技術とコスト効率の高い生産体制を活用し、協力体制のもとで製造されています。</span>
-                            </div>
-                    </div>
-                    </div>
-                <div>
-             
-                    <div class="tags-img-container">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/banner/print1.png" alt="Example Image" class="tags-img ">
-                    <div class="box-1"></div>
-                    <div class="box-2"></div>
-                    <div></div>
-                    </div>
-                </div>
+  <!-- products-tag  -->
+  <div class="sample_inner">
+    <div>
+      <div class="sample-main-title">
+        We Partner With Over 50 High-quality Manufacturers In China
+      </div>
+      <div class="sample_content_wrap">
+        <div>サンプル金型の作成</div>
+        <div class="news_title">
+          <span>
+            <?php
+            include get_template_directory() . '/custom/subBlueLine.php';
+            ?>
+          </span>
+          <span>試作・改良のために設計されており、本格的な生産に入る前にクライアントがデザインを検証できるよう支援します。</span>
+        </div>
 
-<!-- products-tag  -->
-</div>
+
+        <div>サンプル金型の作成</div>
+        <div>
+          <div class="news_title">
+            <span>
+              <?php
+              include get_template_directory() . '/custom/subBlueLine.php';
+              ?>
+            </span>
+            <span>試作・改良のために設計されており、本格的な生産に入る前にクライアントがデザインを検証できるよう支援します。</span>
+          </div></br>
+          <span>サンプル金型は、設計・試作段階でクライアントと密接に連携できるよう、現地で製造されます。量産用金型は、中国や日本の高度な製造技術とコスト効率の高い生産体制を活用し、協力体制のもとで製造されています。</span>
+        </div>
+      </div>
+    </div>
+    <div>
+
+      <div class="tags-img-container">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/banner/print1.png" alt="Example Image" class="tags-img ">
+        <div class="box-1"></div>
+        <div class="box-2"></div>
+        <div></div>
+      </div>
+    </div>
+
+    <!-- products-tag  -->
+  </div>
 </div>
 <!-- sample -->
 
 
 <!-- factories-japan -->
-<div  class="factories_wrapper">
-<div>
-            <?php
-            global $title;
-            $title = "Factories from China";
-                    include get_template_directory() . '/custom/subTitle.php';
-            ?>
-        </div>
-   <!-- container -->
-    <div class="factories_container">
-            <div>
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/banner/banner.png" alt="Example Image" class="mf_img">
-            </div>
-            <div class="mf_list_wrap">
-                <div class="mf_title">Xiamen Xiechangyuan Mold Co., Ltd.</div>
-                    <ul class="ul_flex">
-                            <li>Location: Xiamen, China</li>
-                            <li>Main Operations: Injection mold manufacturing and molding</li>
-                            <li>Established: April 2000</li>
-                            <li>Factory Size: 12,000 m² facility, 130 employees</li>
-                            <li>Certifications: ISO 9001 & ISO 14001</li>
-                            <li>Key Equipment: Injection molding machines (10-1,300 tons, 60 units)</li>
-                            <li>Major Clients: TOYOTA, LG, Midea, Haier, TOTO</li>
-                    </ul>
-            <div class="mf_blur"></div>
-            </div>
+<div class="factories_wrapper">
+  <div>
+    <?php
+    global $title;
+    $title = "Factories from China";
+    include get_template_directory() . '/custom/subTitle.php';
+    ?>
+  </div>
+  <!-- container -->
+  <div class="factories_container">
+    <div>
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/banner/banner.png" alt="Example Image" class="mf_img">
     </div>
-    <!-- container -->
-    <div class="spreator"></div>
-       <!-- container -->
-       <div class="factories_container">
-            <div>
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/banner/banner.png" alt="Example Image" class="mf_img">
-            </div>
-            <div class="mf_list_wrap">
-                <div class="mf_title">Xiamen Ouli Industrial & Trade Co., Ltd.</div>
-                    <ul class="ul_flex">
-                            <li>Location: Xiamen, China</li>
-                            <li>Main Operations: Injection mold manufacturing and molding</li>
-                            <li>Established: April 2000</li>
-                            <li>Factory Size: 12,000 m² facility, 130 employees</li>
-                            <li>Certifications: ISO 9001 & ISO 14001</li>
-                            <li>Key Equipment: Injection molding machines (10-1,300 tons, 60 units)</li>
-                            <li>Major Clients: TOYOTA, LG, Midea, Haier, TOTO</li>
-                    </ul>
-            <div class="mf_blur"></div>
-            </div>
+    <div class="mf_list_wrap">
+      <div class="mf_title">Xiamen Xiechangyuan Mold Co., Ltd.</div>
+      <ul class="ul_flex">
+        <li>Location: Xiamen, China</li>
+        <li>Main Operations: Injection mold manufacturing and molding</li>
+        <li>Established: April 2000</li>
+        <li>Factory Size: 12,000 m² facility, 130 employees</li>
+        <li>Certifications: ISO 9001 & ISO 14001</li>
+        <li>Key Equipment: Injection molding machines (10-1,300 tons, 60 units)</li>
+        <li>Major Clients: TOYOTA, LG, Midea, Haier, TOTO</li>
+      </ul>
+      <div class="mf_blur"></div>
     </div>
-    <!-- container -->
+  </div>
+  <!-- container -->
+  <div class="spreator"></div>
+  <!-- container -->
+  <div class="factories_container">
+    <div>
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/banner/banner.png" alt="Example Image" class="mf_img">
+    </div>
+    <div class="mf_list_wrap">
+      <div class="mf_title">Xiamen Ouli Industrial & Trade Co., Ltd.</div>
+      <ul class="ul_flex">
+        <li>Location: Xiamen, China</li>
+        <li>Main Operations: Injection mold manufacturing and molding</li>
+        <li>Established: April 2000</li>
+        <li>Factory Size: 12,000 m² facility, 130 employees</li>
+        <li>Certifications: ISO 9001 & ISO 14001</li>
+        <li>Key Equipment: Injection molding machines (10-1,300 tons, 60 units)</li>
+        <li>Major Clients: TOYOTA, LG, Midea, Haier, TOTO</li>
+      </ul>
+      <div class="mf_blur"></div>
+    </div>
+  </div>
+  <!-- container -->
 </div>
 <!-- factories-japan -->
 <div class="spreator"></div>
+
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/manufacture.js"></script>
 <?php get_footer(); ?>
--- wp-content/themes/jxmirai/page-service.php
+++ wp-content/themes/jxmirai/page-service.php
@@ -5,16 +5,19 @@
  */
 get_header(); ?>
 <!-- main-banner -->
-<?php
-global $title;
-global $caption;
-global $bgColor;
-$title = "OUR PRODUCT";
-$img = "製造品";
-$bgColor = '#fff';
-$url = "/assets/images/banner/footer.jpg";
-include get_template_directory() . '/custom/pagesBanner.php';
-?>
+
+<div class="product_space">
+  <div class="page-banner-title">
+    <div class="title-wrapper">
+      <div class="line" style="background-color: #000;"></div>
+      <div>
+        <div class="title-text" style="color: #000;">OUR PRODUCTS</div>
+        <span style="color: #000;">製造品</span>
+      </div>
+      <div class="line" style="background-color: #000;"></div>
+    </div>
+  </div>
+</div>
 
 <div class="head-container">
   <div class="head_body">
@@ -28,7 +31,104 @@ include get_template_directory() . '/custom/pagesBanner.php';
     </div>
   </div>
   <div class="head_img">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/service/service.jpg" alt="">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/products/2c1c4e913cee431c880798db58eda0f3.png" alt="">
+  </div>
+</div>
+
+<div class="product_listing_container">
+
+  <div class="product_sidebar_container">
+    <p class="sidebar_title">製品の種類</p>
+    <div class="all_title">全製品</div>
+    <div class="category">
+      <p class="category_title">3Dプリンター</p>
+      <ul class="item_list">
+        <li class="item">光造形</li>
+        <li class="item">FDM造形</li>
+        <li class="item">粉末造形</li>
+        <li class="item">UV硬化造形</li>
+      </ul>
+    </div>
+  </div>
+  <div class="product_listing">
+    <div class="listing">
+      <div class="list_item">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/products/a-1.jpg" alt="">
+        <p>装飾幅木</p>
+        <p>PVC成形</p>
+      </div>
+      <div class="list_item">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/products/a-1.jpg" alt="">
+        <p>装飾幅木</p>
+        <p>PVC成形</p>
+      </div>
+      <div class="list_item">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/products/a-1.jpg" alt="">
+        <p>装飾幅木</p>
+        <p>PVC成形</p>
+      </div><div class="list_item">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/products/a-1.jpg" alt="">
+        <p>装飾幅木</p>
+        <p>PVC成形</p>
+      </div>
+      <div class="list_item">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/products/a-1.jpg" alt="">
+        <p>装飾幅木</p>
+        <p>PVC成形</p>
+      </div>
+      <div class="list_item">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/products/a-1.jpg" alt="">
+        <p>装飾幅木</p>
+        <p>PVC成形</p>
+      </div>
+      <div class="list_item">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/products/a-1.jpg" alt="">
+        <p>装飾幅木</p>
+        <p>PVC成形</p>
+      </div><div class="list_item">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/products/a-1.jpg" alt="">
+        <p>装飾幅木</p>
+        <p>PVC成形</p>
+      </div>
+      <div class="list_item">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/products/a-1.jpg" alt="">
+        <p>装飾幅木</p>
+        <p>PVC成形</p>
+      </div>
+      <div class="list_item">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/products/a-1.jpg" alt="">
+        <p>装飾幅木</p>
+        <p>PVC成形</p>
+      </div>
+      <div class="list_item">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/products/a-1.jpg" alt="">
+        <p>装飾幅木</p>
+        <p>PVC成形</p>
+      </div><div class="list_item">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/products/a-1.jpg" alt="">
+        <p>装飾幅木</p>
+        <p>PVC成形</p>
+      </div>
+      <div class="list_item">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/products/a-1.jpg" alt="">
+        <p>装飾幅木</p>
+        <p>PVC成形</p>
+      </div>
+      <div class="list_item">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/products/a-1.jpg" alt="">
+        <p>装飾幅木</p>
+        <p>PVC成形</p>
+      </div>
+      <div class="list_item">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/products/a-1.jpg" alt="">
+        <p>装飾幅木</p>
+        <p>PVC成形</p>
+      </div><div class="list_item">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/products/a-1.jpg" alt="">
+        <p>装飾幅木</p>
+        <p>PVC成形</p>
+      </div>
+    </div>
   </div>
 </div>
 
