810973a754b2f88fc1a53cd617737b9458ecbbb9
wut <wutmone@o-technique-myanmar.com>
2025-03-07 09:51:10 +0630

    active class for menu

--- wp-content/themes/jxmirai/header.php
+++ wp-content/themes/jxmirai/header.php
@@ -29,11 +29,13 @@
            </div>
            </a>
            <div class="menu-container">
-                <div><a href="<?php echo home_url(); ?>">ホーム</a></div>
-                <div><a href="<?php echo get_permalink( get_page_by_path( 'page-products' ) ) ?>">事業内容 と 製造品</a></div>
-                <div><a href="<?php echo get_permalink( get_page_by_path( 'page-service' ) ) ?>">製造品</a></div>
-                <div><a href="<?php echo get_permalink( get_page_by_path( 'page-manufacture' ) ) ?>">製造拠点</a></div>
-                <div><a href="<?php echo get_permalink( get_page_by_path( 'page-news' ) ) ?>">会社概要</a></div>
+                <div><a href="<?php echo home_url(); ?>" class="<?php echo is_front_page() ? 'menuActive' : ''; ?>">ホーム</a></div>
+                <div><a href="<?php echo get_permalink( get_page_by_path( 'page-products' ) ) ?>" class="<?php echo is_page('page-products') ? 'menuActive' : ''; ?>">事業内容 と 製造品</a></div>
+                <div><a href="<?php echo get_permalink( get_page_by_path( 'page-service' ) ) ?>" class="<?php echo is_page('page-service') ? 'menuActive' : ''; ?>">製造品</a></div>
+                <div><a href="<?php echo get_permalink( get_page_by_path( 'page-manufacture' ) ) ?>" class="<?php echo is_page('page-manufacture') ? 'menuActive' : ''; ?>">製造拠点</a></div>
+                <div><a href="<?php echo get_permalink( get_page_by_path( 'page-news' ) ) ?>" class="<?php echo is_page('page-news') ? 'menuActive' : ''; ?>">会社概要</a></div>
+
+
            </div>
            <div class="sp_menu">
            <!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/images/banner/menu.png" alt="Logo"> -->
