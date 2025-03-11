44f58f92f7e0fdc910e0d949bffbc8d15cf070e3
wut <wutmone@o-technique-myanmar.com>
2025-03-11 10:59:50 +0630

    style

--- wp-content/themes/jxmirai/custom/mainBanner.php
+++ wp-content/themes/jxmirai/custom/mainBanner.php
@@ -15,7 +15,7 @@
             <div class="main-inner-wrap">
                 <div><h2>JX 未来株式会社</h2></div>
                 <div><h1>FUTURE CORPORATION</h1></div>
-                <div class="banner-description"> JX Mirai とのコラボレーションは、品質と顧客満足への取り組みを基盤として、 革新性、精度、コスト効率を融合した企業と提携することを意味します。</div>
+                <div class="banner-description">JXMiraiとの<wbr />コラボレーションは、<wbr />品質と顧客満足への取り組みを<wbr />基盤として、<wbr />革新性、<wbr />精度、<wbr />コスト効率を<wbr />融合した企業と<wbr />提携することを<wbr />意味します。</div>
                 <div class="banner-btn-wrapper">
         <!-- <?php
             include get_template_directory() . '/custom/buttontype1.php';
--- wp-content/themes/jxmirai/header.php
+++ wp-content/themes/jxmirai/header.php
@@ -2,84 +2,84 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-  <meta charset="<?php bloginfo('charset'); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?php bloginfo('name'); ?></title>
-  <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.ico" type="image/x-icon">
-  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.ico" type="image/x-icon">
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php bloginfo('name'); ?></title>
+	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.ico" type="image/x-icon">
 
-  <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/main.css">
-  <script src="<?php echo get_template_directory_uri(); ?>/assets/js/custom-script.js"></script>
-  <!-- <?php wp_head(); ?> -->
+	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/main.css">
+	<script src="<?php echo get_template_directory_uri(); ?>/assets/js/custom-script.js"></script>
+	<!-- <?php wp_head(); ?> -->
 </head>
 
 <body <?php body_class(); ?>>
-  <header>
-    <nav class="nav-container">
-      <!-- <div class="gNav pc">
+	<header>
+		<nav class="nav-container">
+			<!-- <div class="gNav pc">
 				<?php
-        wp_nav_menu();
-        ?>
+				wp_nav_menu();
+				?>
 			</div> -->
-      <div class="nav-wrapper"> <a href="<?php echo home_url(); ?>">
-          <div class="logo-container">
+			<div class="nav-wrapper"> <a href="<?php echo home_url(); ?>">
+					<div class="logo-container">
 
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo/logo.png" alt="Logo">
-                <div class="header_title">
-                    <span>JX未来 式会社</span>
-                    <span>FUTURE CORPORATION</span>
-                </div>
-         
-           </div>
-           </a>
-           <div class="menu-container">
-                <div><a href="<?php echo home_url(); ?>" class="<?php echo is_front_page() ? 'menuActive' : ''; ?>">ホーム</a></div>
-                <div><a href="<?php echo get_permalink( get_page_by_path( 'page-products' ) ) ?>" class="<?php echo is_page('page-products') ? 'menuActive' : ''; ?>">事業内容 と 製造品</a></div>
-                <div><a href="<?php echo get_permalink( get_page_by_path( 'page-service' ) ) ?>" class="<?php echo is_page('page-service') ? 'menuActive' : ''; ?>">製造品</a></div>
-                <div><a href="<?php echo get_permalink( get_page_by_path( 'page-manufacture' ) ) ?>" class="<?php echo is_page('page-manufacture') ? 'menuActive' : ''; ?>">製造拠点</a></div>
-                <div><a href="<?php echo get_permalink( get_page_by_path( 'page-news' ) ) ?>" class="<?php echo is_page('page-news') ? 'menuActive' : ''; ?>">会社概要</a></div>
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo/logo.png" alt="Logo">
+						<div class="header_title">
+							<span>JX未来 式会社</span>
+							<span>FUTURE CORPORATION</span>
+						</div>
+
+					</div>
+				</a>
+				<div class="menu-container">
+					<div><a href="<?php echo home_url(); ?>" class="<?php echo is_front_page() ? 'menuActive' : ''; ?>">ホーム</a></div>
+					<div><a href="<?php echo get_permalink(get_page_by_path('page-products')) ?>" class="<?php echo is_page('page-products') ? 'menuActive' : ''; ?>">事業内容 と 製造品</a></div>
+					<div><a href="<?php echo get_permalink(get_page_by_path('page-service')) ?>" class="<?php echo is_page('page-service') ? 'menuActive' : ''; ?>">製造品</a></div>
+					<div><a href="<?php echo get_permalink(get_page_by_path('page-manufacture')) ?>" class="<?php echo is_page('page-manufacture') ? 'menuActive' : ''; ?>">製造拠点</a></div>
+					<div><a href="<?php echo get_permalink(get_page_by_path('page-news')) ?>" class="<?php echo is_page('page-news') ? 'menuActive' : ''; ?>">会社概要</a></div>
 
 
-           </div>
-           <div class="sp_menu">
-           <!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/images/banner/menu.png" alt="Logo"> -->
-           </div>
-        <button class="menu-btn" id="menuBtn" onclick="toggleMenu()">
-        <div class="bar top"></div>
-        <div class="bar middle"></div>
-        <div class="bar bottom"></div>
-    </button>
+				</div>
+				<div class="sp_menu">
+					<!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/images/banner/menu.png" alt="Logo"> -->
+				</div>
+				<button class="menu-btn" id="menuBtn" onclick="toggleMenu()">
+					<div class="bar top"></div>
+					<div class="bar middle"></div>
+					<div class="bar bottom"></div>
+				</button>
 
-          </div>
-        </a>
-        <!-- <div class="menu-container">
+			</div>
+			</a>
+			<!-- <div class="menu-container">
           <div><a href="<?php echo home_url(); ?>">ホーム</a></div>
           <div><a href="<?php echo get_permalink(get_page_by_path('page-news')) ?>">会社概要</a></div>
           <div><a href="<?php echo get_permalink(get_page_by_path('page-products')) ?>">事業内容 と 製造品</a></div>
           <div><a href="<?php echo get_permalink(get_page_by_path('page-service')) ?>">製造品</a></div>
           <div><a href="<?php echo get_permalink(get_page_by_path('page-manufacture')) ?>">製造拠点</a></div>
         </div> -->
-        <div class="sp_menu">
-          <!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/images/banner/menu.png" alt="Logo"> -->
-        </div>
-        <button class="menu-btn" id="menuBtn" onclick="toggleMenu()">
-          <div class="bar top"></div>
-          <div class="bar middle"></div>
-          <div class="bar bottom"></div>
-        </button>
+			<div class="sp_menu">
+				<!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/images/banner/menu.png" alt="Logo"> -->
+			</div>
+			<button class="menu-btn" id="menuBtn" onclick="toggleMenu()">
+				<div class="bar top"></div>
+				<div class="bar middle"></div>
+				<div class="bar bottom"></div>
+			</button>
 
-        <!-- Sidebar Menu -->
-        <nav class="sidebar" id="sidebar">
-          <ul>
-            <li><a href="<?php echo home_url(); ?>">ホーム</a></li>
-            <li><a href="<?php echo get_permalink(get_page_by_path('page-news')) ?>">会社概要</a></li>
-            <li><a href="<?php echo get_permalink(get_page_by_path('page-products')) ?>">事業内容 と 製造品</a></li>
-            <li><a href="<?php echo get_permalink(get_page_by_path('page-service')) ?>">製造品</a></li>
-            <li><a href="<?php echo get_permalink(get_page_by_path('page-manufacture')) ?>">製造拠点</a></li>
-          </ul>
-        </nav>
-      </div>
+			<!-- Sidebar Menu -->
+			<nav class="sidebar" id="sidebar">
+				<ul>
+					<li><a href="<?php echo home_url(); ?>">ホーム</a></li>
+					<li><a href="<?php echo get_permalink(get_page_by_path('page-news')) ?>">会社概要</a></li>
+					<li><a href="<?php echo get_permalink(get_page_by_path('page-products')) ?>">事業内容 と 製造品</a></li>
+					<li><a href="<?php echo get_permalink(get_page_by_path('page-service')) ?>">製造品</a></li>
+					<li><a href="<?php echo get_permalink(get_page_by_path('page-manufacture')) ?>">製造拠点</a></li>
+				</ul>
+			</nav>
+			</div>
 
-    </nav>
-  </header>
+		</nav>
+	</header>
